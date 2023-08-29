0936307e9aa35d65a25ca5c27d5794d51fa34004
Hrithik <slumdog4321@gmail.com>
2023-08-29 12:01:47 +0530

    codemirror added and editor component created

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import CodeMirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
 
 const Editor = () => {
-  return <div className="flex justify-center items-center font-bold">Editor Goes Here...</div>;
+  const shouldLog = useRef(true);
+  useEffect(() => {
+    if (shouldLog.current) {
+      shouldLog.current = false;
+      async function init() {
+        CodeMirror.fromTextArea(document.getElementById("realTimeEditor"), {
+          mode: { name: "javascript", json: true },
+          theme: "dracula",
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers: true,
+        });
+      }
+      init();
+    }
+    // async function init() {
+    //   CodeMirror.fromTextArea(document.getElementById("realTimeEditor"), {
+    //     mode: { name: "javascript", json: true },
+    //     theme: "dracula",
+    //     autoCloseTags: true,
+    //     autoCloseBrackets: true,
+    //     lineNumbers: true,
+    //   });
+    // }
+    // init();
+  }, []);
+  return <textarea className="w-screen text-white" id="realTimeEditor"></textarea>;
 };
 
 export default Editor;
